4fcc966ed0ef8503c00be75ecc795db76d62a68e
marco cobian <marco.cobian@coppel.com>
2023-03-24 20:01:53 -0600

    changes server name

--- src/app.py
+++ src/app.py
@@ -25,5 +25,5 @@ def upload_image():
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
 
